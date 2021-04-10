9533614a6b4c3f899e467e25bda1ad03db023d58
cemanaral <cemanaral425@gmail.com>
2021-04-10 11:09:38 +0300

    project files

---
+++ main.py
@@ -0,0 +1,15 @@
+# Cem Anaral 150119761
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from main import *
+
+class TestProject(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
