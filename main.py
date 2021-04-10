bb6cdd437936cbe451cd10be49d0e36a8c116a24
cemanaral <cemanaral425@gmail.com>
2021-04-10 15:02:06 +0300

    implemented take_complement(), started to signed_to_binary()

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ is_unsigned    = lambda line: 'u' in line
 is_float       = lambda line: '.' in line
 is_big_end     = lambda symbol: symbol == 'b'
 is_little_end  = lambda symbol: symbol == 'l'
-
+is_negative    = lambda number: number[0] == '-'
 
 
 def unsigned_to_binary(number: str):
     """Converts base-10 unsigned int to binary"""   
     binary = ''
-
-    # Conversion algorithm
     decimal = int(number.rstrip('u\n'))
+    
+    # Conversion algorithm
     while decimal > 0:
         binary = str(decimal % 2) + binary
         decimal = int(decimal // 2)
@@ -30,10 +30,35 @@ def unsigned_to_binary(number: str):
     # If input is bigger than UMax
     elif len(binary) > INT_SIZE_BITS:
         raise OverflowError(f"{number} can not be represented in {INT_SIZE} bytes.")
-
     return binary
     
+def take_complement(binary: str):
+    "Takes complement of binary number"
+    list_binary = list(binary)
 
+    for i in range(len(list_binary)):
+        bit = list_binary[i]
+        if bit == '0':
+            list_binary[i] = '1'
+        else:
+            list_binary[i] = '0'
+    return ''.join(list_binary)
+
+
+def signed_to_binary(number: str):
+    """Converts base-10 signed int to binary (two's complement) using 'Subtract Powers of Two' method"""
+
+    # If number is positive or zero
+    if not is_negative(number): 
+        return '0' + unsigned_to_binary(number)[1:]
+
+    # if number is negative
+    binary = ['_'] * INT_SIZE_BITS
+    binary[0] = '1'
+    decimal = int(number.strip())
+
+
+    return ''.join(binary)
 
 def evaluate(line, byte_ordering, float_size):
     """Evaluates read line"""
@@ -49,7 +74,9 @@ def evaluate(line, byte_ordering, float_size):
     
     # Else, signed int
     else:
-        print("Decimal Signed int:", line)
+        print("Decimal Signed int:\t", line.strip())
+        print("Binary Signed int:\t", signed_to_binary(line))
+    
     print("------------------------------------------------")
     
 
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from main import *
-
+"""
 res = unsigned_to_binary("65535u")
 print(res)
 assert len(res) == INT_SIZE_BITS
 # assert res == "0000000110011100"
+"""
+
+print(take_complement("11010"))
